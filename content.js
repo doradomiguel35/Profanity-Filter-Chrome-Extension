8d8fbe03ef82a167491ea8af9e64d89d83d0a96c
unknown <doradomiguel35@gmail.com>
2018-07-03 12:25:24 +0800

    Can censor profanity but still case sensitive

---
+++ content.js
@@ -0,0 +1,48 @@
+console.log("UnseeIt V1.0 Ready");
+var defaultWords = ['ass','asses','asshole',
+					'bastard','bitch','blowjob',
+					'cunt','clitoris','creampie','cum',
+					'damn','dick','douche','douchebag','deepthroat',
+					'fuck','fvck',
+					'handjob',
+					'motherfucker',
+					'orgy',
+					'piss','pissed',
+					'slut','shit','son of a bitch',
+					'tits',
+					'whore'];
+
+var innerBody = document.getElementsByTagName("*");
+var profanityCount = 0;
+
+var word = defaultWords[0];
+
+
+for (var k = 0; k < defaultWords.length; k++) {
+	var words = defaultWords[k];
+	for (var i = 0; i < innerBody.length; i++) {
+		var element = innerBody[i];
+		
+		for (var j = 0; j < element.childNodes.length; j++) {
+			var node = element.childNodes[j];
+			
+				if (node.nodeType === 3) {
+					
+					var text = node.nodeValue;
+					// console.log(node.nodeValue);
+
+					var replacedText = text.replace(defaultWords[k], '****');
+					
+					// console.log(replacedText);
+					if (replacedText != text) {
+						element.replaceChild(document.createTextNode(replacedText), node);
+						profanityCount++;
+					}
+					
+				}
+			
+		}
+	}
+}
+
+console.log(profanityCount);
