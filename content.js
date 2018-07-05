e8b13878fa2d025cb726a7c324591d8ef09093dc
unknown <doradomiguel35@gmail.com>
2018-07-05 14:24:53 +0800

    case-insensitive, fixed webpage being broken

--- content.js
+++ content.js
@@ -3,9 +3,7 @@ console.log("UnseeIt V1.0 Ready");
 var defaultWords = ['ass','asses','asshole','assshit','ass-hat',
 					'assbag','assbite','asscock','assfuck','asshead',
 					'asslick','asslicker','assmonkey','assmunch','asssucker',
-<<<<<<< HEAD
-					'bastard','bitch','blowjob','bampot','bitchass',
-					'bitchy','bullshit',
+					'bastard','bitch','blowjob','bampot','bitchass','bitchy','bullshit',
 					'cunt','clitoris','creampie','cum','clitface',
 					'clusterfuck','cockass','cockbite','cockburger','cockface',
 					'cockhead','cockmonkey','cocknose','cocknugget','cockshit',
@@ -17,17 +15,15 @@ var defaultWords = ['ass','asses','asshole','assshit','ass-hat',
 					'dickweed','dickwod','dipshit','doochbag','douchefag',
 					'dumass','dumb ass','dumbass','dumbfuck','dumbshit','dumshit',
 					'fagfucker','fuck','fvck','fuckass',
-=======
 					'bastard','bitch','blowjob',
 					'cunt','creampie','cum',
 					'damn','dick','douchebag','deepthroat',
 					'fuck','fvck',
->>>>>>> 028b43b2f04ab00dec7cd538c2e50b75107f9731
 					'handjob',
 					'motherfucker',
 					'orgy',
 					'piss','pissed','pissing',
-					'slut','shit','son of a bitch',
+					'slut','shit',
 					'tits',
 					'whore'];
 
@@ -44,20 +40,16 @@ for (var k = 0; k < defaultWords.length; k++) {
 		
 		for (var j = 0; j < element.childNodes.length; j++) {
 			var node = element.childNodes[j];
-				
+			
 			if (node.nodeType === 3) {
 				var text = node.nodeValue;
-				// console.log(text.search(defaultWords[k]))
-				// if(defaultWords[k] === text.search(defaultWords[k])){
-				var replacedText = text.toLowerCase().replace(defaultWords[k], '****');
-				// if (replacedText != text) {
-
+				var wordRegex = new RegExp(defaultWords[k],"i");
+				var replacedText = text.replace(wordRegex, '****');
 				element.replaceChild(document.createTextNode(replacedText), node);
 				profanityCount++;
-			// }
-				
-			
 			}
+			
+			
 		}
 	}	
 }
