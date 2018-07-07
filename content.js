240e04a8f48920bfcaad0e2f7e235144826b34b5
unknown <doradomiguel35@gmail.com>
2018-07-07 14:15:46 +0800

    substitution method under construction

--- content.js
+++ content.js
@@ -1,6 +1,5 @@
 console.log("UnseeIt V1.0 Ready");
-
-
+var filterMethod = 2; 
 var defaultWords = ["asses","asshole","assshit","ass-hat",,"asssucker",
 					"assbag","assbite","asscock","assfuck","asshead",
 					"asslick","asslicker","assmonkey","assmunch","ass",
@@ -24,11 +23,61 @@ var defaultWords = ["asses","asshole","assshit","ass-hat",,"asssucker",
 					"shite","shit", 		
 					"whore"];
 
+var substituteWords = {
+	"asses":"butts","asshole":"butthole",
+	"assshit":"buttard","ass-hat":"butt-hat",
+	"asssucker":"buttsipper","assbag":"buttbag",
+	"assbite":"buttbite","asscock":"buttcrook",
+	"assfuck":"buttmate","asshead":"butthead",
+	"asslick":"buttlick","asslicker":"buttlicker",
+	"assmonkey":"buttmonkey","assmunch":"butmunch",
+	"ass":"butt","bastard":"no father",
+	// "blowjob":{[""]},"bampot":{[""]},
+	// "bitchass":{[""]},"bitchy":{[""]},
+	// "bullshit":{[""]},"bitch":{[""]},
+	// "cunt":{[""]},
+	// "creampie":{[""]},"cum":{[""]},
+	// "clitface":{[""]},"clusterfuck":{[""]},
+	// "cockass":{[""]},"cockbite":{[""]},
+	// "cockburger":{[""]},"cockface":{[""]},
+	// "cockhead":{[""]},"cockmonkey":{[""]},
+	// "cocknose":{[""]},"cocknugget":{[""]},
+	// "cockshit":{[""]},"cockwaffle":{[""]},
+	// "cumbubble":{[""]},"cumslut":{[""]},
+	// "cumtart":{[""]},"cuntass":{[""]},
+	// "cumdumpster":{[""]},"cuntface":{[""]},
+	// "cuntrag":{[""]},"cuntslut":{[""]},
+	// "cock":{[""]},"damn":{[""]},
+	// "douche":{[""]},"douchebag":{[""]},
+	// "deepthroat":{[""]},"dickbag":{[""]},
+	// "dickface":{[""]},"dickfuck":{[""]},
+	// "dickfucker":{[""]},"dickhead":{[""]},
+	// "dickjuice":{[""]},"dickmilk":{[""]},
+	// "dicksucker":{[""]},"dickwad":{[""]},
+	// "dickweasel":{[""]},"dickweed":{[""]},
+	// "dickwod":{[""]},"dipshit":{[""]},
+	// "doochbag":{[""]},"douchefag":{[""]},
+	// "dumass":{[""]},"dumb ass":{[""]},
+	// "dumbass":{[""]},"dumbfuck":{[""]},
+	// "dumbshit":{[""]},"dumshit":{[""]},
+	// "dick":{[""]},"fagfucker":{[""]},
+	// "fvck":{[""]},"fucker":{[""]},
+	// "fuckers":{[""]},"fucks":{[""]},
+	// "fucken":{[""]},"fucking":{[""]},
+	// "fuckass":{[""]},"fuck":{[""]},
+	// "handjob":{[""]},
+	// "holyshit":{[""]},
+	// "motherfucker":{[""]},
+	// "orgy":{[""]},
+	// "piss":{[""]},"pissed":{[""]},
+	// "pissing":{[""]},
+	// "shite":{[""]},"shit":{[""]}, 		
+	// "whore":{[""]}
+};
 var innerBody = document.getElementsByTagName("*");
 var profanityCount = 0;
-var wordSplit = [];
 var word = defaultWords[0];
-var filterMethod = 0; 
+var res = [];
 
 function filterWords(){
 	for (var k = 0; k < defaultWords.length; k++) {
@@ -42,26 +91,48 @@ function filterWords(){
 					var wordRegex = new RegExp("\\b"+defaultWords[k]+"\\b","gi");
 					switch(filterMethod){		
 						case 0://Censor
+							replaceWords(text,element,wordRegex,"****",node);
+							profanityCount++;
+							break;
+						case 1://Substitute 
 							if(wordRegex.test(text) === true){
-								var replacedText = text.replace(wordRegex, "****");
-								element.replaceChild(document.createTextNode(replacedText), node);
+								var word = text.replace(wordRegex,defaultWords[k]);
+								res = text.match(defaultWords[k]);
+								// if(defaultWords[k] === res[l]){
+								// 	replaceWords(text,element,wordRegex,replaceAndSubstitute(defaultWords[k]),node);
+								// 	profanityCount++;
+								// }
+								// var wordSubstitute = replaceAndSubstitute(word);
+								// replaceWords(text,element,wordRegex,wordSubstitute,node);
+								console.log(res);
 								profanityCount++;
 							}
-							break;
-						case 1://Substitute
 							break;
 						case 2://Remove 
-							if(wordRegex.test(text) === true){
-								var replacedText = text.replace(wordRegex, "");
-								element.replaceChild(document.createTextNode(replacedText), node);
-								profanityCount++;
-							}
+							replaceWords(text,element,wordRegex,"",node);
+							profanityCount++;
 							break;
+
 					}
 				}
 			}
 		}	
 	}
 }
+
+function replaceWords(text,element,wordRegex,replace,node){
+	if(wordRegex.test(text) === true){
+		var replacedText = text.replace(wordRegex, replace);
+		element.replaceChild(document.createTextNode(replacedText), node);
+	}
+}
+
+function replaceAndSubstitute(word){
+	var replace = substituteWords.word;
+	return replace;
+}
+
+
 filterWords();
+
 console.log("Number of words filtered: "+profanityCount);
