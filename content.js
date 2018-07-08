04826ce1f90c632028fd228a3d1bf21e6a0ca9ad
unknown <doradomiguel35@gmail.com>
2018-07-08 22:21:13 +0800

    can now substitute words on some tabs and websites, still under construction

--- content.js
+++ content.js
@@ -1,37 +1,39 @@
 console.log("UnseeIt V1.0 Ready");
-var filterMethod = 2; 
-var defaultWords = ["asses","asshole","assshit","ass-hat",,"asssucker",
-					"assbag","assbite","asscock","assfuck","asshead",
-					"asslick","asslicker","assmonkey","assmunch","ass",
-					"bastard","blowjob","bampot","bitchass","bitchy",
-					"bullshit","bitch",
-					"cunt","clitoris","creampie","cum","clitface",
-					"clusterfuck","cockass","cockbite","cockburger","cockface",
-					"cockhead","cockmonkey","cocknose","cocknugget","cockshit",
-					"cockwaffle","cumbubble","cumslut","cumtart","cuntass",
-					"cumdumpster","cuntface","cuntrag","cuntslut","cock",
-					"damn","douche","douchebag","deepthroat",
-					"dickbag","dickface","dickfuck","dickfucker","dickhead",
-					"dickjuice","dickmilk","dicksucker","dickwad","dickweasel",
-					"dickweed","dickwod","dipshit","doochbag","douchefag",
-					"dumass","dumb ass","dumbass","dumbfuck","dumbshit","dumshit","dick",
-					"fagfucker","fvck","fucker","fuckers","fucks","fucken","fucking","fuckass","fuck",
-					"handjob","holyshit",
-					"motherfucker",
-					"orgy",
-					"piss","pissed","pissing",
-					"shite","shit", 		
-					"whore"];
+var filterMethod = 1; 
+var defaultWords = [
+	"asses","asshole","assshit","ass-hat",,"asssucker",
+	"assbag","assbite","asscock","assfuck","asshead",
+	"asslick","asslicker","assmonkey","assmunch","ass",
+	"bastard","blowjob","bampot","bitchass","bitchy",
+	"bullshit","bitch",
+	"cunt","clitoris","creampie","cum","clitface",
+	"clusterfuck","cockass","cockbite","cockburger","cockface",
+	"cockhead","cockmonkey","cocknose","cocknugget","cockshit",
+	"cockwaffle","cumbubble","cumslut","cumtart","cuntass",
+	"cumdumpster","cuntface","cuntrag","cuntslut","cock",
+	"damn","douche","douchebag","deepthroat",
+	"dickbag","dickface","dickfuck","dickfucker","dickhead",
+	"dickjuice","dickmilk","dicksucker","dickwad","dickweasel",
+	"dickweed","dickwod","dipshit","doochbag","douchefag",
+	"dumass","dumb ass","dumbass","dumbfuck","dumbshit","dumshit","dick",
+	"fagfucker","fvck","fucker","fuckers","fucks","fucken","fucking","fuckass","fuck",
+	"handjob","holyshit",
+	"motherfucker",
+	"orgy",
+	"piss","pissed","pissing",
+	"shite","shit", 		
+	"whore"
+];
 
 var substituteWords = {
-	"asses":"butts","asshole":"butthole",
-	"assshit":"buttard","ass-hat":"butt-hat",
-	"asssucker":"buttsipper","assbag":"buttbag",
-	"assbite":"buttbite","asscock":"buttcrook",
-	"assfuck":"buttmate","asshead":"butthead",
-	"asslick":"buttlick","asslicker":"buttlicker",
-	"assmonkey":"buttmonkey","assmunch":"butmunch",
-	"ass":"butt","bastard":"no father",
+	"asses":{"substitute":"[butts]"},"asshole":{"substitute":"[butthole]"},
+	"assshit":{"substitute":"[buttard]"},"ass-hat":{"substitute":"[butt-hat]"},
+	"asssucker":{"substitute":"[buttsipper]"},"assbag":{"substitute":"[buttbag]"},
+	"assbite":{"substitute":"[buttbite]"},"asscock":{"substitute":"[buttcrook]"},
+	"assfuck":{"substitute":"[buttmate]"},"asshead":{"substitute":"[butthead]"},
+	"asslick":{"substitute":"[buttlick]"},"asslicker":{"substitute":"[buttlicker]"},
+	"assmonkey":{"substitute":"[buttmonkey]"},"assmunch":{"substitute":"[butmunch]"},
+	"ass":{"substitute":"[butt]"},"bastard":{"substitute":"[no father]"}
 	// "blowjob":{[""]},"bampot":{[""]},
 	// "bitchass":{[""]},"bitchy":{[""]},
 	// "bullshit":{[""]},"bitch":{[""]},
@@ -74,10 +76,10 @@ var substituteWords = {
 	// "shite":{[""]},"shit":{[""]}, 		
 	// "whore":{[""]}
 };
+
 var innerBody = document.getElementsByTagName("*");
 var profanityCount = 0;
 var word = defaultWords[0];
-var res = [];
 
 function filterWords(){
 	for (var k = 0; k < defaultWords.length; k++) {
@@ -92,25 +94,24 @@ function filterWords(){
 					switch(filterMethod){		
 						case 0://Censor
 							replaceWords(text,element,wordRegex,"****",node);
-							profanityCount++;
 							break;
 						case 1://Substitute 
 							if(wordRegex.test(text) === true){
 								var word = text.replace(wordRegex,defaultWords[k]);
-								res = text.match(defaultWords[k]);
-								// if(defaultWords[k] === res[l]){
-								// 	replaceWords(text,element,wordRegex,replaceAndSubstitute(defaultWords[k]),node);
-								// 	profanityCount++;
-								// }
-								// var wordSubstitute = replaceAndSubstitute(word);
-								// replaceWords(text,element,wordRegex,wordSubstitute,node);
-								console.log(res);
-								profanityCount++;
-							}
+								var res = word.match(wordRegex);
+								for(var matchedWord of res){
+									if(defaultWords[k] === matchedWord ){
+										var textReplace = text.replace(wordRegex, replaceAndSubstitute(matchedWord));
+										element.replaceChild(document.createTextNode(textReplace), node);
+										profanityCount++
+									}
+								}
+							}	
+
+							
 							break;
 						case 2://Remove 
 							replaceWords(text,element,wordRegex,"",node);
-							profanityCount++;
 							break;
 
 					}
@@ -121,14 +122,12 @@ function filterWords(){
 }
 
 function replaceWords(text,element,wordRegex,replace,node){
-	if(wordRegex.test(text) === true){
-		var replacedText = text.replace(wordRegex, replace);
-		element.replaceChild(document.createTextNode(replacedText), node);
-	}
+	var replacedText = text.replace(wordRegex, replace);
+	element.replaceChild(document.createTextNode(replacedText), node);
 }
 
 function replaceAndSubstitute(word){
-	var replace = substituteWords.word;
+	var replace = substituteWords[word]["substitute"];
 	return replace;
 }
 
