4dfa9614c6a37ee11d8161315ebcd1f98abea780
unknown <doradomiguel35@gmail.com>
2018-07-25 10:19:23 +0800

    working on the filter toggle function

--- popup.js
+++ popup.js
@@ -1,1 +1,15 @@
+// function toggleFilter(){
+// 	chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
+//     var activeTab = tabs[0];
+//     chrome.tabs.getSelected(null, function(tab) {
+// 	  var code = 'window.location.reload();';
+// 	  chrome.tabs.executeScript(tab.id, {code: code});
+// 	});
+
+//     chrome.tabs.sendMessage(activeTab.id, {"message": "filter"});
+// 	});
+// }
+
+
+// document.getElementById('toggleFilter').addEventListener('change',)
 document.getElementById('options').addEventListener('click', function() {chrome.runtime.openOptionsPage(); });
