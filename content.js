1b5d667c7d28255cfeb5cbf8cb2b6bbdd67023d7
unknown <doradomiguel35@gmail.com>
2018-07-31 13:41:33 +0800

    added censor character options

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-console.log("UnseeIt V1.0 Ready");
-
 var defaultWords = [
 	"asses","asshole","assshit","ass-hat",,"asssucker",
 	"assbag","assbite","asscock","assfuck","asshead",
@@ -26,14 +24,15 @@ var defaultWords = [
 ];
 
 var substituteWords = {
-	"asses":{"substitute":"[butts]"},"asshole":{"substitute":"[butthole]"},
-	"assshit":{"substitute":"[buttard]"},"ass-hat":{"substitute":"[butt-hat]"},
-	"asssucker":{"substitute":"[buttsipper]"},"assbag":{"substitute":"[buttbag]"},
-	"assbite":{"substitute":"[buttbite]"},"asscock":{"substitute":"[buttcrook]"},
-	"assfuck":{"substitute":"[buttmate]"},"asshead":{"substitute":"[butthead]"},
-	"asslick":{"substitute":"[buttlick]"},"asslicker":{"substitute":"[buttlicker]"},
-	"assmonkey":{"substitute":"[buttmonkey]"},"assmunch":{"substitute":"[butmunch]"},
-	"ass":{"substitute":"[butt]"},"bastard":{"substitute":"[no father]"}
+	// "asses":{"substitute":["[butts]"]},"asshole":{"substitute":["[butthole]"]},
+	// "assshit":{"substitute":["[buttard]"]},"ass-hat":{"substitute":["[butt-hat]"]},
+	// "asssucker":{"substitute":["[buttsipper]"]},"assbag":{"substitute":["[buttbag]"]},
+	// "assbite":{"substitute":["[buttbite]"]},"asscock":{"substitute":["[buttcrook]"]},
+	// "assfuck":{"substitute":["[buttmate]"]},"asshead":{"substitute":["[butthead]"]},
+	// "asslick":{"substitute":["[buttlick]"]},"asslicker":{"substitute":["[buttlicker]"]},
+	// "assmonkey":{"substitute":["[buttmonkey]"]},"assmunch":{"substitute":["[butmunch]"]},
+	// "ass":{"substitute":["[butt]"]},
+	// "bastard":{"substitute":["[no father]"]}
 	// "blowjob":{[""]},"bampot":{[""]},
 	// "bitchass":{[""]},"bitchy":{[""]},
 	// "bullshit":{[""]},"bitch":{[""]},
@@ -75,31 +74,45 @@ var substituteWords = {
 	// "pissing":{[""]},
 	// "shite":{[""]},"shit":{[""]}, 		
 	// "whore":{[""]}
+	'asses':'[butts]','asshole':'[butthole]',
+	'assshit':'[buttard]','ass-hat':'[butt-hat]',
+	'asssucker':'[buttsipper]','assbag':'[buttbag]',
+	'assbite':'[buttbite]','asscock':'[buttcrook]',
+	'assfuck':'[buttmate]','asshead':'[butthead]',
+	'asslick':'[buttlick]','asslicker':'[buttlicker]',
+	'assmonkey':'[buttmonkey]','assmunch':'[butmunch]',
+	'ass':'[butt]','bastard':'[no father]'
 };
 
 var innerBody = document.getElementsByTagName("*");
 var profanityCount = 0;
 var filterMethod = 0; 
 var matchMethod = 1;
-var censorCharacter = "****";
+var censorCharacter = 8;
 var wordRegex;
 
+console.log("UnseeIt V1.0 Ready");
+// var word = defaultWords[0];
+// console.log(replaceAndSubstitute(word));
+
 function filterWords(){
+	var urlname = window.location.pathname;
+	console.log(urlname);
 	for (var k = 0; k < defaultWords.length; k++) {
 		var words = defaultWords[k];
-		
 		for (var i = 0; i < innerBody.length; i++) {
 		  var element = innerBody[i];
-			
 			for (var j = 0; j < element.childNodes.length; j++) {
 				var node = element.childNodes[j];
-				
 				if (node.nodeType === 3) {
 					var text = node.nodeValue;
 					var wordRegexMethod = globalMatchMethods(matchMethod,defaultWords[k]);
-					switchFilterMethods(filterMethod,text,element,wordRegexMethod,censorCharacter,node,profanityCount,defaultWords);
 					
-				}
+					var word = defaultWords[k];
+					// console.log(word);		
+					switchFilterMethods(filterMethod,text,element,wordRegexMethod,node,profanityCount,word);
+					
+				} 
 			}
 		}	
 	}
@@ -112,23 +125,19 @@ function globalMatchMethods(matchMethod,defaultWords){
 			wordRegexMethod = new RegExp("\\b"+defaultWords+"\\b",'gi');
 			break;
 		case 1://Per Word
-			// wordRegexMethod = new RegExp('(' + defaultWords + ')' + escapeRegExp(defaultWords.slice(1)),'gi'); 
 			wordRegexMethod = new RegExp('(' + defaultWords + ')','gi');
 			break;
 	}
 	return wordRegexMethod;
 }
 
-function escapeRegExp(str) {
-  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
-}
-
-function switchFilterMethods(filterMethod,text,element,wordRegex,censorCharacter,node,defaultWords){
+function switchFilterMethods(filterMethod,text,element,wordRegex,node,defaultWords){
 	switch(filterMethod){		
 		case 0://Censor
-			censorWord(text,element,wordRegex,censorCharacter,node);
+			censorWord(text,element,wordRegex,node);
 			break;
 		case 1://Substitute 
+			// console.log(defaultWords);
 			substituteWord(text,element,wordRegex,node,defaultWords);
 			break;
 		case 2://Remove 
@@ -137,41 +146,78 @@ function switchFilterMethods(filterMethod,text,element,wordRegex,censorCharacter
 	}
 }
 
-function substituteWord(text, element, wordRegex, node, defaultWords){
+//Substitute Method
+function substituteWord(text,element,wordRegex,node,defaultWords){
+	// if(wordRegex.test(text) === true){
+	// 	var replaceWord = text.replace(wordRegex,defaultWords);
+	// 	var res = replaceWord.match(wordRegex);
+	// 	console.log(res);
+	// 	for(var matchedWord of res){
+	// 		if(defaultWords === matchedWord ){
+	// 			var textReplace = text.replace(wordRegex, replaceAndSubstitute(matchedWord));
+	// 			element.replaceChild(document.createTextNode(textReplace), node);
+	// 			profanityCount++;
+	// 		}
+	// 	}
+	// }	
 	if(wordRegex.test(text) === true){
-		var word = text.replace(wordRegex,defaultWords);
-		var res = word.match(wordRegex);
-		for(var matchedWord of res){
-			if(defaultWords === matchedWord ){
-				var textReplace = text.replace(wordRegex, replaceAndSubstitute(matchedWord));
-				element.replaceChild(document.createTextNode(textReplace), node);
-				profanityCount++;
-			}
-		}
-	}	
-}
-
-function censorWord(text,element,wordRegex,character,node) {
-	if(wordRegex.test(text) === true){
-		var replacedWord = replaceWords(text,element,wordRegex,character,node);
-		profanityCount++;
+		var substitutedWord = replaceAndSubstitute(defaultWords);
+		console.log(defaultWords);
+		replaceWords(text,element,wordRegex,substitutedWord,node);
 	}
 }
 
+//Censor Method
+function censorWord(text,element,wordRegex,node) {
+	if(wordRegex.test(text) === true){
+		var character;
+		switch(censorCharacter){
+			case 0:
+				replaceWords(text,element,wordRegex,"****",node);
+				break;
+			case 1:
+				replaceWords(text,element,wordRegex,"&&&&",node);
+				break;
+			case 2:
+				replaceWords(text,element,wordRegex,"$$$$$$$$",node);
+				break;
+			case 3:
+				replaceWords(text,element,wordRegex,"####",node);
+				break;
+			case 4:
+				replaceWords(text,element,wordRegex,"@@@@",node);
+				break;
+			case 5:
+				replaceWords(text,element,wordRegex,"^^^^",node);
+				break;
+			case 6:
+				replaceWords(text,element,wordRegex,"----",node);
+				break;
+			case 7:
+				replaceWords(text,element,wordRegex,"____",node);
+				break;
+			case 8:
+				replaceWords(text,element,wordRegex,"*&$#",node);
+				break;
+		}
+	}
+}
+
+//Remove Method
 function removeWord(text,element,wordRegex,node){
 	if(wordRegex.test(text) === true){
-		var replaceWord = replaceWords(text,element,wordRegex,"",node);
-		profanityCount++;
+		replaceWords(text,element,wordRegex,"",node);
 	}
 }
 
 function replaceWords(text,element,wordRegex,replace,node){
 	var replacedText = text.replace(wordRegex, replace);
 	element.replaceChild(document.createTextNode(replacedText), node);
+	profanityCount++;
 }
 
 function replaceAndSubstitute(word){
-	var replace = substituteWords[word].substitute;
+	var replace = substituteWords[word];
 	return replace;
 }
 
