ef92dffeb6f71427285a519f26ed00f3564867c9
unknown <doradomiguel35@gmail.com>
2018-09-13 10:59:09 +0800

    3 methods done stil needs polishing, recording of number of profane words in websites done

--- content.js
+++ content.js
@@ -2,6 +2,7 @@ var defaultWords = [
 	"asses","asshole","assshit","ass-hat",,"asssucker",
 	"assbag","assbite","asscock","assfuck","asshead",
 	"asslick","asslicker","assmonkey","assmunch","ass",
+	"anal",
 	"bastard","blowjob","bampot","bitchass","bitchy",
 	"bullshit","bitch",
 	"cunt","creampie","cum","clitface",
@@ -9,12 +10,13 @@ var defaultWords = [
 	"cockhead","cockmonkey","cocknose","cocknugget","cockshit",
 	"cockwaffle","cumbubble","cumslut","cumtart","cuntass",
 	"cumdumpster","cuntface","cuntrag","cuntslut","cock",
-	"damn","douche","douchebag","deepthroat",
+	"damn","douche","douchebag","deepthroat","dildo","dildos",
 	"dickbag","dickface","dickfuck","dickfucker","dickhead",
 	"dickjuice","dickmilk","dicksucker","dickwad","dickweasel",
 	"dickweed","dickwod","dipshit","doochbag","douchefag",
 	"dumass","dumb ass","dumbass","dumbfuck","dumbshit","dumshit","dick",
-	"fagfucker","fvck","fucker","fuckers","fucks","fucken","fucking","fuckass","fuck",
+	"fagfucker","fvck","fucker","fuckers","fucks","fucken","fucking","fuckass","fucked","fuck","fuckin",
+	"fck",
 	"handjob","holyshit",
 	"motherfucker",
 	"orgy",
@@ -24,64 +26,19 @@ var defaultWords = [
 ];
 
 var substituteWords = {
-	// "asses":{"substitute":["[butts]"]},"asshole":{"substitute":["[butthole]"]},
-	// "assshit":{"substitute":["[buttard]"]},"ass-hat":{"substitute":["[butt-hat]"]},
-	// "asssucker":{"substitute":["[buttsipper]"]},"assbag":{"substitute":["[buttbag]"]},
-	// "assbite":{"substitute":["[buttbite]"]},"asscock":{"substitute":["[buttcrook]"]},
-	// "assfuck":{"substitute":["[buttmate]"]},"asshead":{"substitute":["[butthead]"]},
-	// "asslick":{"substitute":["[buttlick]"]},"asslicker":{"substitute":["[buttlicker]"]},
-	// "assmonkey":{"substitute":["[buttmonkey]"]},"assmunch":{"substitute":["[butmunch]"]},
-	// "ass":{"substitute":["[butt]"]},
-	// "bastard":{"substitute":["[no father]"]}
-	// "blowjob":{[""]},"bampot":{[""]},
-	// "bitchass":{[""]},"bitchy":{[""]},
-	// "bullshit":{[""]},"bitch":{[""]},
-	// "cunt":{[""]},
-	// "creampie":{[""]},"cum":{[""]},
-	// "clitface":{[""]},"clusterfuck":{[""]},
-	// "cockass":{[""]},"cockbite":{[""]},
-	// "cockburger":{[""]},"cockface":{[""]},
-	// "cockhead":{[""]},"cockmonkey":{[""]},
-	// "cocknose":{[""]},"cocknugget":{[""]},
-	// "cockshit":{[""]},"cockwaffle":{[""]},
-	// "cumbubble":{[""]},"cumslut":{[""]},
-	// "cumtart":{[""]},"cuntass":{[""]},
-	// "cumdumpster":{[""]},"cuntface":{[""]},
-	// "cuntrag":{[""]},"cuntslut":{[""]},
-	// "cock":{[""]},"damn":{[""]},
-	// "douche":{[""]},"douchebag":{[""]},
-	// "deepthroat":{[""]},"dickbag":{[""]},
-	// "dickface":{[""]},"dickfuck":{[""]},
-	// "dickfucker":{[""]},"dickhead":{[""]},
-	// "dickjuice":{[""]},"dickmilk":{[""]},
-	// "dicksucker":{[""]},"dickwad":{[""]},
-	// "dickweasel":{[""]},"dickweed":{[""]},
-	// "dickwod":{[""]},"dipshit":{[""]},
-	// "doochbag":{[""]},"douchefag":{[""]},
-	// "dumass":{[""]},"dumb ass":{[""]},
-	// "dumbass":{[""]},"dumbfuck":{[""]},
-	// "dumbshit":{[""]},"dumshit":{[""]},
-	// "dick":{[""]},"fagfucker":{[""]},
-	// "fvck":{[""]},"fucker":{[""]},
-	// "fuckers":{[""]},"fucks":{[""]},
-	// "fucken":{[""]},"fucking":{[""]},
-	// "fuckass":{[""]},"fuck":{[""]},
-	// "handjob":{[""]},
-	// "holyshit":{[""]},
-	// "motherfucker":{[""]},
-	// "orgy":{[""]},
-	// "piss":{[""]},"pissed":{[""]},
-	// "pissing":{[""]},
-	// "shite":{[""]},"shit":{[""]}, 		
-	// "whore":{[""]}
-	'asses':'[butts]','asshole':'[butthole]',
+	'asses':'[butts]','asshole':'[butthole]',		
 	'assshit':'[buttard]','ass-hat':'[butt-hat]',
 	'asssucker':'[buttsipper]','assbag':'[buttbag]',
 	'assbite':'[buttbite]','asscock':'[buttcrook]',
 	'assfuck':'[buttmate]','asshead':'[butthead]',
 	'asslick':'[buttlick]','asslicker':'[buttlicker]',
 	'assmonkey':'[buttmonkey]','assmunch':'[butmunch]',
-	'ass':'[butt]','bastard':'[no father]'
+	'ass':'[butt]','anal':'[butt]','bastard':'[no father]',
+	'dildo':'[toy]','dildos':'[toys]',
+	'fuck':'[love]','fucks':'[loves]','fucker':'[lover]',
+	'fuckable':'[loveable]','fucked':'[loved]','fuckin':'[lovin]',
+	'fvck':'[lve]','fck':'[lve]'
+
 };
 
 var settings = {
@@ -93,36 +50,65 @@ var settings = {
 
 var innerBody = document.getElementsByTagName("*");
 var profanityCount = 0;
-var filterMethod,censorCharacter,filterToggle,matchMethod;
+var filterMethod,censorCharacter,filterToggle,matchMethod,site;
 var matchMethod = 0;
 var wordRegex;
+var stringifyObject;
+var retrieveCount;
+var regexpSite;
 
 console.log("UnseeIt V1.0 Ready");
 // var word = defaultWords[0];
 // console.log(replaceAndSubstitute(word));
 
 function loadSettings(){
+	var websites= [];
 	chrome.storage.sync.get(settings,function(settings){
 		filterMethod = settings.filterMethod;
 		censorCharacter = settings.censorCharacter;
 		filterToggle = settings.filterToggle;
 		matchMethod = settings.matchMethod;
-		console.log("Filter Method:"+filterMethod+",\n"+"Censor Character:"+censorCharacter+",\n"+"Filter Toggle:"+filterToggle+",\n"+"Match Method"+matchMethod);
+		var origin_site = document.location.origin;
+
+		console.log("Filter Method:"+filterMethod+",\n"+"Censor Character:"+censorCharacter+",\n"+"Filter Toggle:"+filterToggle+",\n"+"Match Method: "+matchMethod);
 		toggleFilter();
+		console.log("Number of words filtered: "+profanityCount);
+		chrome.storage.sync.get(['websites'],function(result){
+			stringifyObject = JSON.stringify(result.websites);
+			regexpSite = new RegExp(origin_site);
+
+			for(var i = 0;i < result.websites.length; i++){
+				site = result.websites[i].site;
+				regexpSite = new RegExp(origin_site);
+				//Check if site is existing 
+				if(origin_site === site){
+					console.log(origin_site);
+					stringifyObject = JSON.stringify(result.websites);
+					var replaceObject = stringifyObject.replace(result.websites[i].count,function(){
+							retrieveCount = parseInt(result.websites[i].count);
+							retrieveCount+=profanityCount;
+							return retrieveCount.toString();
+					});
+					websites = JSON.parse(replaceObject);
+					console.log(websites);
+					chrome.storage.sync.set({websites},function(){
+						console.log("Key Overwritten");
+
+						websites.push(JSON.parse(replaceObject));
+					});
+				}
+			}
+
+			//Check if site is non-existing
+			if(regexpSite.test(JSON.stringify(websites))!= true){
+					addWebStatistics(origin_site,profanityCount);
+			}
+		});
 	});
-	
-	// console.log()
-	// chrome.storage.sync.get("censorCharacter",function(settings){
-	// 	censorCharacter = settings.censorCharacter;
-	// 	console.log(censorCharacter);
-	// });
-	
 }
 
 function filterWords(){
-	console.log("function executed");
 	for (var k = 0; k < defaultWords.length; k++) {
-		// console.log("interated");
 		var words = defaultWords[k];
 		for (var i = 0; i < innerBody.length; i++) {
 		  var element = innerBody[i];
@@ -131,20 +117,40 @@ function filterWords(){
 				if (node.nodeType === 3) {
 					var text = node.nodeValue;
 					var wordRegexMethod = globalMatchMethods(matchMethod,defaultWords[k]);
-					switchFilterMethods(filterMethod,text,element,wordRegexMethod,node,profanityCount,defaultWords[k]);
-					
+					//Censor/Remove methods
+					if(filterMethod != "1"){ 
+						switchFilterMethods(filterMethod,text,element,wordRegexMethod,node,profanityCount,defaultWords[k]);
+					}
+
+					else{
+						//Substitute method
+						substituteWord(text,element,wordRegexMethod,node,defaultWords[k]);
+					}
 				} 
 			}
 		}	
 	}
 }
 
+function addWebStatistics(site, profanityCount){
+	var websites = [];
+	chrome.storage.sync.get(['websites'],function(result){
+		for(var i = 0 ; i < result.websites.length ; i++){
+				websites.push(result.websites[i]);
+		}
+		websites.push({"site":site, "count":profanityCount.toString()});
+		console.log(JSON.stringify(websites));
+		chrome.storage.sync.set({websites},function(){
+			websites.push({"site":site, "count":profanityCount.toString()});
+			console.log("Keys set");
+		});
+	});
+}
+
 function globalMatchMethods(matchMethod,defaultWords){
 	var wordRegexMethod;
-
 	switch(matchMethod){
 		case "0"://Match Word
-			
 			wordRegexMethod = new RegExp("\\b"+defaultWords+"\\b",'gi');
 			break;
 		case "1"://Per Word
@@ -159,10 +165,6 @@ function switchFilterMethods(filterMethod,text,element,wordRegex,node,defaultWor
 		case "0"://Censor
 			censorWord(text,element,wordRegex,node);
 			break;
-		case "1"://Substitute 
-			// console.log(defaultWords);
-			substituteWord(text,element,wordRegex,node,defaultWords);
-			break;
 		case "2"://Remove 
 			removeWord(text,element,wordRegex,node);
 			break;
@@ -171,22 +173,8 @@ function switchFilterMethods(filterMethod,text,element,wordRegex,node,defaultWor
 
 //Substitute Method
 function substituteWord(text,element,wordRegex,node,defaultWords){
-	// if(wordRegex.test(text) === true){
-	// 	var replaceWord = text.replace(wordRegex,defaultWords);
-	// 	var res = replaceWord.match(wordRegex);
-	// 	console.log(res);
-	// 	for(var matchedWord of res){
-	// 		if(defaultWords === matchedWord ){
-	// 			var textReplace = text.replace(wordRegex, replaceAndSubstitute(matchedWord));
-	// 			element.replaceChild(document.createTextNode(textReplace), node);
-	// 			profanityCount++;
-	// 		}
-	// 	}
-	// }	
-
 	if(wordRegex.test(text) === true){
-		var substitutedWord = replaceAndSubstitute(defaultWords);
-		console.log(defaultWords);
+		var substitutedWord = substituteWords[defaultWords];
 		replaceWords(text,element,wordRegex,substitutedWord,node);
 	}
 }
@@ -242,21 +230,15 @@ function replaceWords(text,element,wordRegex,replace,node){
 
 function replaceAndSubstitute(word){
 	var replace = substituteWords[word];
+	console.log(replace);
 	return replace;
 }
 
 function toggleFilter(){
-	// chrome.storage.sync.get(['filterToggle'], function (result){
-	// 	var toggle = result.filterToggle;
-	// 	console.log(toggle);
-		console.log(filterToggle);
-
-		if(filterToggle === true){
-			filterWords();
-		}
-	// });
+	if(filterToggle === true){
+		filterWords();
+	}
 }
 
 loadSettings();
-// toggleFilter();
-console.log("Number of words filtered: "+profanityCount);
+
