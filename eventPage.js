d926a9f012c64c36cd019ec7fef313367fd602e7
unknown <doradomiguel35@gmail.com>
2018-09-14 15:27:16 +0800

    Testing if keys are set upon installation

--- eventPage.js
+++ eventPage.js
@@ -1,19 +1,35 @@
-chrome.runtime.onInstalled.addListener(function() {
-	var defaultSettings = {
+chrome.runtime.onInstalled.addListener(function(details){
+    var defaultSettings = {
 		censorCharacter: "****",
 		filterMethod: "1",
 		filterToggle: true,
 		matchMethod: "0",
 		password: "null", 
 		warningDomains: [
-			"www.facebook.com",
-			"www.twitter.com",
-			"www.pornhub.com",
-			"www.9gag.com",
-			"www.tumblr.com"
-		]
+			"https://www.facebook.com",
+			"https://www.twitter.com",
+			"https://www.9gag.com",
+			"https://www.tumblr.com",
+			"https://www.youtube.com",
+			"https://m.youtube.com"
+		],
+			"websites": [
+			{"count": "3358","site": "https://www.google.com.ph"},
+			{"count": "249","site": "https://notifications.google.com"},
+			{"count": "143","site": "https://www.youtube.com"},
+			{"count": "128","site": "https://www-urbandictionary-com.cdn.ampproject.org"},
+			{"count": "118","site": "https://stackoverflow.com"}
+			]
 	}
-    chrome.storage.sync.set(defaultSettings, function() {
+
+    if(details.reason == "install"){
+       chrome.storage.sync.set(defaultSettings, function() {
       	console.log("Extension successfully installed");
     });
+    }
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+	
+    
   });
