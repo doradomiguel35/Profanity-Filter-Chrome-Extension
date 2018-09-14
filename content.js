638da8d0f2434a06021ab4ef1f088d3790eeb923
unknown <doradomiguel35@gmail.com>
2018-09-14 15:27:58 +0800

    75% complete

--- content.js
+++ content.js
@@ -45,7 +45,23 @@ var settings = {
 	"filterMethod": 1,
 	"censorCharacter": "*",
 	"filterToggle": true,
-	"matchMethod": 0
+	"matchMethod": 0,
+	"password": "null",
+	"warningDomains": [
+			"https://www.facebook.com",
+			"https://www.twitter.com",
+			"https://www.9gag.com",
+			"https://www.tumblr.com",
+			"https://www.youtube.com",
+			"https://m.youtube.com"
+	],
+	"websites": [
+			{"count": "3358","site": "https://www.google.com.ph"},
+			{"count": "249","site": "https://notifications.google.com"},
+			{"count": "143","site": "https://www.youtube.com"},
+			{"count": "128","site": "https://www-urbandictionary-com.cdn.ampproject.org"},
+			{"count": "118","site": "https://stackoverflow.com"}
+		]
 };
 
 var innerBody = document.getElementsByTagName("*");
@@ -142,7 +158,7 @@ function addWebStatistics(site, profanityCount){
 		console.log(JSON.stringify(websites));
 		chrome.storage.sync.set({websites},function(){
 			websites.push({"site":site, "count":profanityCount.toString()});
-			console.log("Keys set");
+			console.log(websites);
 		});
 	});
 }
