d44bd62acef1addc295fa611eac8e88d8cffd789
unknown <doradomiguel35@gmail.com>
2018-09-18 14:31:43 +0800

    80% complete

--- eventPage.js
+++ eventPage.js
@@ -11,15 +11,32 @@ chrome.runtime.onInstalled.addListener(function(details){
 			"https://www.9gag.com",
 			"https://www.tumblr.com",
 			"https://www.youtube.com",
-			"https://m.youtube.com"
 		],
-			"websites": [
+		websites: [
 			{"count": "3358","site": "https://www.google.com.ph"},
 			{"count": "249","site": "https://notifications.google.com"},
 			{"count": "143","site": "https://www.youtube.com"},
 			{"count": "128","site": "https://www-urbandictionary-com.cdn.ampproject.org"},
 			{"count": "118","site": "https://stackoverflow.com"}
-			]
+			],
+		defaultWord: [
+			{"count": 0,"word": "fuck"},
+			{"count": 0,"word": "fuckable"},
+			{"count": 0,"word": "fucked"},
+			{"count": 0,"word": "fucker"},
+			{"count": 0,"word": "fuckin"},
+			{"count": 0,"word": "fucks"},
+			{"count": 0,"word": "fvck"}
+		],
+		substituteWords: [
+			{"substitute": "[love]","word": "fuck"},
+			{"substitute": "[loveable]","word": "fuckable"},
+			{"substitute": "[loved]","word": "fucked"},
+			{"substitute": "[lover]","word": "fucker"},
+			{"substitute": "[lovin]","word": "fuckin"},
+			{"substitute": "[loves]","word": "fucks"},
+			{"substitute": "[lve]","word": "fvck"}
+		]
 	}
 
     if(details.reason == "install"){
