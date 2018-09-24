7191dad7c40433907d9c7048e7a8c5ddf110eecb
unknown <doradomiguel35@gmail.com>
2018-09-24 13:33:38 +0800

    can now filter in all websites

--- content.js
+++ content.js
@@ -6,96 +6,174 @@ var settings = {
 	"password": "null",
 };
 
-var innerBody = document.querySelectorAll('body *')
-var filterMethod,censorCharacter,filterToggle,matchMethod,site;
 var matchMethod = 0;
-var wordRegex;
-var stringifyObject;
-var retrieveCount;
-var regexpSite;
 var profanityCount = 0;
-var words = [];
+var filterMethod,censorCharacter,filterToggle,matchMethod;
+var wordRegex,stringifyObject,retrieveCount,regexpSite,node,wordRegex;
+var xpathDocText = '//*[not(self::script or self::style)]/text()[normalize-space(.) != ""]';
+var xpathNodeText = './/*[not(self::script or self::style)]/text()[normalize-space(.) != ""]';
+var defaultWords = [];
+var substituteWords = [];
+var websites = [];
+var origin_site = document.location.origin;
 
+
+
+var innerBody = document.querySelectorAll('*');
 
 console.log("UnseeIt V1.0 Ready");
-
+	
 function loadSettings(){
 	chrome.storage.sync.get(settings,function(settings){
-		filterMethod = settings.filterMethod;
-		censorCharacter = settings.censorCharacter;
-		filterToggle = settings.filterToggle;
-		matchMethod = settings.matchMethod;
-		toggleFilter();
-	});
-}
-
-function filterWords(){
-	var origin_site = document.location.origin;
-	var websites;
-	chrome.storage.sync.get(['defaultWords'],function(result){
-		chrome.storage.sync.get(['substituteWords'],function(sub){
-			for (var k = 0; k < result.defaultWords.length; k++) {
-				var words = result.defaultWords[k].word;
-				for (var i = 0; i < innerBody.length; i++) {
-				  var element = innerBody[i];
-					for (var j = 0; j < element.childNodes.length; j++) {
-						var node = element.childNodes[j];
-						var text = node.nodeValue;
-						var defaultWords = result.defaultWords;
-						var wordRegexMethod = globalMatchMethods(matchMethod,result.defaultWords[k].word);
-						//Censor/Remove methods
-						if(filterMethod != "1"){ 
-							switchFilterMethods(filterMethod,text,element,wordRegexMethod,node,result.defaultWords[k].word,defaultWords);
-						}
-
-						else{
-							//Substitute method
-							if(wordRegexMethod.test(text) === true){
-								for(var i = 0; i < sub.substituteWords.length; i++){
-									var objectWord = sub.substituteWords[i].word;
-									var substitutedWord = sub.substituteWords[i].substitute;
-									if(objectWord === result.defaultWords[k].word){
-										replaceWords(text, element, wordRegexMethod,substitutedWord,node,result.defaultWords[k].word,defaultWords);
-									}
-								}
-							}
-						}
-						
-					}
-				}	
-			}
-
-			chrome.storage.sync.get(['websites'],function(result){
-				console.log("Number of words filtered: "+profanityCount);
-				stringifyObject = JSON.stringify(result.websites);
-				regexpSite = new RegExp(origin_site);
-
-				for(var i = 0;i < result.websites.length; i++){
-					site = result.websites[i].site;
-					regexpSite = new RegExp(origin_site);
-
-					//Check if site is existing 
-					if(origin_site === site){
-						stringifyObject = JSON.stringify(result.websites);
-						var replaceObject = stringifyObject.replace(result.websites[i].count,function(){
-								retrieveCount = parseInt(result.websites[i].count);
-								retrieveCount+=profanityCount;
-								return retrieveCount.toString();
-						});
-						websites = JSON.parse(replaceObject);
-						chrome.storage.sync.set({websites},function(){
-							websites.push(JSON.parse(replaceObject));
-						});
-					}
-				}
-
-				//Check if site is non-existing
-				if(regexpSite.test(JSON.stringify(websites))!= true){
-						addWebStatistics(origin_site,profanityCount);
-				}
+		chrome.storage.sync.get(['defaultWords'],function(result){
+			chrome.storage.sync.get(['substituteWords'],function(sub){
+				chrome.storage.sync.get(['websites'],function(site){
+					defaultWords = result.defaultWords;
+					substituteWords = sub.substituteWords;
+					websites = site.websites;
+					filterMethod = settings.filterMethod;
+					censorCharacter = settings.censorCharacter;
+					filterToggle = settings.filterToggle;
+					matchMethod = settings.matchMethod;
+					profanityCount = 0;
+					origin_site = document.location.origin;
+					toggleFilter();
+				});
 			});
 		});
 	});
+}
+
+function mutationObserver(){
+	var observerConfig = {
+    childList: true,
+    subtree: true
+  };
+
+  // When DOM is modified, remove profanity from inserted node
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      checkNodeForProfanity(mutation);
+    });
+  });
+
+
+  // Remove profanity from new objects
+  observer.observe(document, observerConfig);
+}
+
+function checkNodeForProfanity(mutation) {
+  mutation.addedNodes.forEach(function(node) {
+    // console.log(isForbiddenNode(node));
+    if (!isForbiddenNode(node)) {
+      filterWords(xpathNodeText, node);
+    }
+
+  });
+}
+
+function isForbiddenNode(node) {
+  return node.isContentEditable || // DraftJS and many others
+  (node.parentNode && node.parentNode.isContentEditable) || // Special case for Gmail
+  (node.tagName && (node.tagName.toLowerCase() == "textarea" || // Some catch-alls
+                    node.tagName.toLowerCase() == "input" ||
+                    node.tagName.toLowerCase() == "script" ||
+                    node.tagName.toLowerCase() == "style")
+  );
+}
+
+function filterWords(xpathExpression, node){
+	node = (typeof node !== 'undefined') ?  node : document;
+  var evalResult = document.evaluate(
+    xpathExpression,
+    node,
+    null,
+    XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,
+    null
+  );
+
+  for (var i = 0; i < evalResult.snapshotLength; i++) {
+    var textNode = evalResult.snapshotItem(i);
+    textNode.data = replaceText(textNode.data);
+  }
+
+  
+
+}
+
+function checkWebsite(){
+	stringifyObject = JSON.stringify(websites);
+	regexpSite = new RegExp(origin_site);
+
+	for(var i = 0;i < websites.length; i++){
+		regexpSite = new RegExp(origin_site);
+
+		//Check if site is existing 
+		if(origin_site === websites[i].site){
+			stringifyObject = JSON.stringify(websites);
+			var replaceObject = stringifyObject.replace(websites[i].count,function(){
+					retrieveCount = parseInt(websites[i].count);
+					retrieveCount+=profanityCount;
+					return retrieveCount.toString();
+			});
+			websites = JSON.parse(replaceObject);
+			chrome.storage.sync.set({websites},function(){
+				websites.push(JSON.parse(replaceObject));
+			});
+		}
+	}
+
+	//Check if site is non-existing
+	if(regexpSite.test(JSON.stringify(websites))!= true){
+			addWebStatistics(origin_site,profanityCount);
+	}
+}
+
+function replaceText(text){
+	switch(filterMethod){
+		case "0"://Censor Method
+			for(var i = 0; i < defaultWords.length; i++){
+				// console.log(text+"==="+result.defaultWords[i].word);
+				var wordRegex = globalMatchMethods(matchMethod,defaultWords[i].word);
+				if(wordRegex.test(text) === true){
+					text = text.replace(wordRegex, censorCharacter);
+					profanityCount++;
+					let temp = defaultWords.find(e => e.word === defaultWords[i].word);
+					temp.count+=1;
+					chrome.storage.sync.set({defaultWords},function(){});
+				}
+			}
+			
+			break;
+		case "1": //Substitute Method
+			for(var i = 0; i < substituteWords.length; i++){
+				var wordRegex = globalMatchMethods(matchMethod,substituteWords[i].word);
+				if(wordRegex.test(text) === true){
+					text = text.replace(wordRegex, substituteWords[i].substitute);
+					profanityCount++;
+					let temp = defaultWords.find(e => e.word === substituteWords[i].word);
+					console.log(temp+=1);
+					chrome.storage.sync.set({defaultWords},function(){});
+				}
+			}
+			
+			break;
+		case "2": //Remove Method
+			for(var i = 0; i < defaultWords.length; i++){
+				var wordRegex = globalMatchMethods(matchMethod,defaultWords[i].word);
+				if(wordRegex.test(text) === true){
+					text = text.replace(wordRegex, "");
+					profanityCount++;
+					let temp = defaultWords.find(e => e.word === defaultWords[i].word);
+					temp.count+=1;
+					chrome.storage.sync.set({defaultWords},function(){});
+				}
+			}
+			chrome.storage.sync.set({defaultWords},function(){});
+			break;
+	}
+	
+	return text;
 }
 
 function addWebStatistics(site, profanityCount){
@@ -147,90 +225,13 @@ function switchFilterMethods(filterMethod,text,element,wordRegex,node,word,defau
 	}
 }
 
-//Censor Method
-function censorWord(text,element,wordRegex,node,word,defaultWords){
-	if(wordRegex.test(text) === true){
-		switch(censorCharacter){
-			case "****":
-				replaceWords(text,element,wordRegex,"****",node,word,defaultWords);
-				break;
-			case "&&&&":
-				replaceWords(text,element,wordRegex,"&&&&",node,word,defaultWords);
-				break;
-			case "$$$$":
-				replaceWords(text,element,wordRegex,"$$$$$$$$",node,word,defaultWords);
-				break;
-			case "####":
-				replaceWords(text,element,wordRegex,"####",node,word,defaultWords);
-				break;
-			case "@@@@":
-				replaceWords(text,element,wordRegex,"@@@@",node,word,defaultWords);
-				break;
-			case "^^^^":
-				replaceWords(text,element,wordRegex,"^^^^",node,word,defaultWords);
-				break;
-			case "----":
-				replaceWords(text,element,wordRegex,"----",node,word,defaultWords);
-				break;
-			case "____":
-				replaceWords(text,element,wordRegex,"____",node,word,defaultWords);
-				break;
-			case "mixed":
-				replaceWords(text,element,wordRegex,"*&$#",node,word,defaultWords);
-				break;
-		}
-	}
-
-}
-
-//Remove Method
-function removeWord(text,element,wordRegex,node,word,defaultWords){
-	if(wordRegex.test(text) === true){
-		profanityCount++;
-		replaceWords(text,element,wordRegex,"",node,word,defaultWords);
-	}
-}
-
-function replaceWords(text,element,wordRegex,replace,node,word,defaultWords){
-	profanityCount++;
-	var wordCount;
-	var replacedText = text.replace(wordRegex, replace);
-	element.replaceChild(document.createTextNode(replacedText), node);
-	
-	let temp = defaultWords.find(e => e.word === word);
-	temp.count+=1;
-	
-	chrome.storage.sync.set({defaultWords},function(){
-		console.log(defaultWords);
-	});
-
-	
-}
-
-function assignKey(obj, key) {
-  typeof obj[key] === 'undefined' ? obj[key] = 1 : obj[key]++;
-}
-
-function findIndex(array, attr, value){
-	for(var i = 0; i < array.length; i += 1) {
-        if(array[i][attr] === value) {
-            return i; 
-        }
-    }
-    return -1;
-}
-
-function replaceAndSubstitute(word){
-	var replace = substituteWords[word];
-	console.log(replace);
-	return replace;
-}
-
 function toggleFilter(){
 	if(filterToggle === true){
-		filterWords();
+		filterWords(xpathDocText);
+		checkWebsite();
+		checkWarningDomain();
+		mutationObserver();
 	}
 }
 
-checkWarningDomain();
 loadSettings();
